c4cc4edb31374c337072be8040126f64931a89d3
Rico Kahler <ricokahler@me.com>
2017-11-07 07:57:41 -0500

    add test for out of order segments

--- test/tests.ts
+++ test/tests.ts
@@ -2,7 +2,7 @@ import 'mocha';
 import { expect } from 'chai';
 
 import * as uuid from 'uuid/v4';
-import { range } from 'lodash';
+import { range, shuffle } from 'lodash';
 import { Observable, Observer, ReplaySubject } from 'rxjs';
 import { oneLine } from 'common-tags';
 
@@ -20,7 +20,7 @@ describe('Cumulative acknowledgement', function () {
   it(
     oneLine`
       Upon receiving a segment from the server, the client will respond with a cumulative
-      acknowledgment.
+      acknowledgment, which follows the standard protocol defined for TCP 
     `,
     async function () {
       const messageId = uuid();
@@ -65,13 +65,70 @@ describe('Cumulative acknowledgement', function () {
         dataSegmentStream,
         segmentSizeInBytes,
         sendAckSegment,
-        logger: console.log.bind(console),
+        // logger: console.log.bind(console), // uncomment to enable logging
       });
 
       const message = (await messageStream.take(1).toPromise()).toString();
 
       expect(message).to.be.equal(exampleMessage);
     }
+  );
+});
+
+describe('Out-of-order and duplicate and in-order segments', function () {
+  it(
+    oneLine`
+      Upon receiving a segment, if the client detects a gap (out of order), buffer it; if it is
+      duplication or corruption, just discard it; otherwise (in order) print out the content as
+      shown before.
+    `,
+    async function () {
+
+      const messageId = uuid();
+
+      const buffer = new Buffer(exampleMessage);
+      let dataSegmentsToSend = (range(buffer.byteLength / segmentSizeInBytes)
+        .map(i => i * segmentSizeInBytes)
+        .map(seq => {
+          const dataSegment: DataSegment = {
+            messageId,
+            seq,
+            data: buffer.slice(seq, seq + segmentSizeInBytes)
+          };
+          return dataSegment;
+        })
+      );
+      dataSegmentsToSend.push({
+        seq: dataSegmentsToSend.length * segmentSizeInBytes,
+        messageId,
+        last: true,
+        data: new Buffer(''),
+      });
+
+      dataSegmentsToSend = shuffle(dataSegmentsToSend); // out of order
+
+      async function sendAckSegment(ackSegment: AckSegment) {
+        // nothing to do
+      }
+
+      const dataSegmentStream = Observable.of(...dataSegmentsToSend);
+
+      const messageStream = createReceiver({
+        dataSegmentStream,
+        segmentSizeInBytes,
+        sendAckSegment,
+        // logger: console.log.bind(console), // uncomment to enable logging
+      });
+
+      const message = (await messageStream.take(1).toPromise()).toString();
+      expect(message).to.be.equal(exampleMessage);
+    }
+  );
+});
+
+describe('Fast retransmit', function () {
+  it(
+    ``
   );
 });
 
